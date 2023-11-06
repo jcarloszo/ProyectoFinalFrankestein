ec1726659f5f620367bb284b29b0eeb4d640272b
Ana Santillan <anisantillan10@gmail.com>
2023-11-06 05:51:09 -0300

    se corrigieron detalles esteticos

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,60 @@
-// import Container from 'react-bootstrap/Container';
-// import Nav from 'react-bootstrap/Nav';
-// import Navbar from 'react-bootstrap/Navbar';
-// import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../src/assets/Logo.png"
-// import Button from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
 
 
-// function App() {
-//   return (
-//     <>  
-//     <Navbar expand="lg" className="bg-primary" data-bs-theme="dark">
-//       <Container>
-//         <Navbar.Brand href="#home">SIPROSA</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
-//           <Nav className="ml-auto">
-//             <Nav.Link href="#home">Inicio</Nav.Link>
-//             <Nav.Link href="#link">Acceder</Nav.Link>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//     <section className='m-5'>
-//       <h1 className='text-center display-5'>Control de Permanencia en Hospitales</h1>
-//       <div className='text-center'>
-//         <img className="p-2 imagenInicio" src={Logo} alt="Logo App" />
-//       </div>
-//       <div>
-//       <div className="d-grid gap-2">
-//       <Button variant="outline-primary" size="lg">
-//         Formularios
-//       </Button>
-//       <Button variant="outline-primary" size="lg">
-//         Informes
-//       </Button>
-//       <Button variant="outline-primary" size="lg">
-//         Agentes
-//       </Button>
-//       <Button variant="outline-primary" size="lg">
-//         Tareas
-//       </Button>
-//     </div>
-//       </div>
-//     </section>
+function App() {
+  return (
+    <>  
+    <Navbar expand="lg" className="bg-primary" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">SIPROSA</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Acceder</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    <section className='m-5'>
+      <h1 className='text-center display-5'>Control de Permanencia en Hospitales</h1>
+      <div className='text-center'>
+        <img className="p-2 imagenInicio" src={Logo} alt="Logo App" />
+      </div>
+      <div>
+      <div className="d-grid gap-2">
+      <Button variant="outline-primary" size="lg">
+        Formularios
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Informes
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Agentes
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Tareas
+      </Button>
+    </div>
+      </div>
+    </section>
 
-//     <div className="bg-primary text-light pt-3">
-//       <Container>
-//         <div className="pt-2 text-center">
-//           <p>&copy; Todos los derechos reservados</p>
-//         </div>
-//       </Container>
-//     </div>
+    <div className="bg-primary text-light pt-3">
+      <Container>
+        <div className="pt-2 text-center">
+          <p>&copy; Todos los derechos reservados</p>
+        </div>
+      </Container>
+    </div>
 
-//     </>
-//   );
-// }
+    </>
+  );
+}
 
-// export default App;
+export default App;
 
