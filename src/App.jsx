d17a053064e397a9643711060a364621e9e01237
Ana Santillan <anisantillan10@gmail.com>
2023-11-06 03:53:39 -0300

    se agrego las opciones del menu

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,28 @@ function App() {
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  
+    <section className='m-5'>
+      <h1 className='text-center display-4'>Control de Permanencia en Hospitales</h1>
+      <div className='text-center'>
+        <img className="w-50 p-2" src={Logo} alt="Logo App" />
+      </div>
+      <div>
+      <div className="d-grid gap-2">
+      <Button variant="outline-primary" size="lg">
+        Formularios
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Informes
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Agentes
+      </Button>
+      <Button variant="outline-primary" size="lg">
+        Tareas
+      </Button>
+    </div>
+      </div>
+    </section>
 
     <div className="bg-body-tertiary text-dark pt-3">
       <Container>
