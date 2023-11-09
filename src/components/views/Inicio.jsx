bda0e79e296bf1cb85138632105d1086d5e6d75f
Carlos Zurita <jcarlos_zo@outlook.com>
2023-11-08 21:02:11 -0300

    try pwa failed

--- src/components/views/Inicio.jsx
+++ src/components/views/Inicio.jsx
@@ -2,8 +2,48 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Logo from "../../assets/Logo.png"
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
 
 function Inicio() {
+    const [isReadyForInstall, setIsReadyForInstall] = React.useState(false);
+
+    useEffect(() => {
+        window.addEventListener("beforeinstallprompt", (event) => {
+            // Prevent the mini-infobar from appearing on mobile.
+            //event.preventDefault();
+            console.log("👍", "beforeinstallprompt", event);
+            // Stash the event so it can be triggered later.
+            window.deferredPrompt = event;
+            // Remove the 'hidden' class from the install button container.
+            setIsReadyForInstall(true);
+        });
+    }, []);
+
+    async function downloadApp() {
+        console.log("👍", "butInstall-clicked");
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+          // The deferred prompt isn't available.
+          console.log("oops, no prompt event guardado en window");
+          return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        const result = await promptEvent.userChoice;
+        console.log("👍", "userChoice", result);
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+        // Hide the install button.
+        setIsReadyForInstall(false);
+      }
+
+    useEffect(() => {
+        console.log(isReadyForInstall);
+    }, [isReadyForInstall]);
+
     return (
         <>
             <section className='m-5'>
@@ -11,6 +51,13 @@ function Inicio() {
                 <div className='text-center'>
                     <img className="p-2 imagenInicio" src={Logo} alt="Logo App" />
                 </div>
+                {
+                    isReadyForInstall ? 
+                    <>
+                    <button type="button" className="btn btn-outline-primary" onClick={downloadApp}>Descargar Aplicación</button>
+                    </>
+                    : <></>
+                }
                 <div>
                     <div className="d-grid gap-2">
                         <Link
