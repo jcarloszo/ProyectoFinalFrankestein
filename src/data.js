c2be8e17965717b62e0a48ee42c6c55c47f8f674
AgustinSanchez1 <agustinsanchezdev@gmail.com>
2023-12-04 12:46:53 -0300

    cambios

--- src/data.js
+++ src/data.js
@@ -1,4 +1,93 @@
 const datos = [
-    {"documento":859617,"nombre":"IRMA NELIDA","apellido":"DIAZ DE PEREZ","email":"IRMA NELIDADIAZ DE PEREZ@gmail.com","telefono":123456789,"iddependencia":1406,"dependencia":"SERVICIO NEONATOLOGIA-EMERGENCIA"},{"documento":1568398,"nombre":"JANET RUDELLS","apellido":"TRONCOSO DE GAFFET","email":"JANET RUDELLSTRONCOSO DE GAFFET@gmail.com","telefono":123456789,"iddependencia":54,"dependencia":"DIRECCION GENERAL DE FISCALIZACION SANITARIA"},{"documento":2510748,"nombre":"FLORA DE JESUS","apellido":"CORREA DE LOBO","email":"FLORA DE JESUSCORREA DE LOBO@gmail.com","telefono":123456789,"iddependencia":5,"dependencia":"SERVICIOS GENERALES"},{"documento":2789385,"nombre":"LUISA FRANCISCA","apellido":"AYBAR DE CRUZ","email":"LUISA FRANCISCAAYBAR DE CRUZ@gmail.com","telefono":123456789,"iddependencia":1375,"dependencia":"UNIDAD NEONATOLOGIA"},{"documento":2925749,"nombre":"MAXIMA ELSA","apellido":"ORTIZ","email":"MAXIMA ELSAORTIZ@gmail.com","telefono":123456789,"iddependencia":542,"dependencia":"AO AGUILARES"},{"documento":3717926,"nombre":"ROSA","apellido":"CHABEN","email":"ROSACHABEN@gmail.com","telefono":123456789,"iddependencia":1386,"dependencia":"UNIDAD LABORATORIO CENTRAL"},{"documento":3725736,"nombre":"LIDIA POLONIA","apellido":"MOYA","email":"LIDIA POLONIAMOYA@gmail.com","telefono":123456789,"iddependencia":368,"dependencia":"SALA V"},{"documento":3778889,"nombre":"SONIA DE LOS ANGELES","apellido":"OJEDA","email":"SONIA DE LOS ANGELESOJEDA@gmail.com","telefono":123456789,"iddependencia":63,"dependencia":"CAPS SAN BERNARDO"},{"documento":3808251,"nombre":"ANGELICA LETICIA","apellido":"ROMANO DE GOMEZ","email":"ANGELICA LETICIAROMANO DE GOMEZ@gmail.com","telefono":123456789,"iddependencia":1446,"dependencia":"DIRECCION HOSPITAL DE CLINICAS NUESTRA SEÑORA DEL CARMEN"},{"documento":3925161,"nombre":"HERMELINDA JESUS","apellido":"RODRIGUEZ DE ESPECHE","email":"HERMELINDA JESUSRODRIGUEZ DE ESPECHE@gmail.com","telefono":123456789,"iddependencia":681,"dependencia":"OBSTETRICIA"}
+    {
+        "documento": 10000001, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 100, 
+        "dependencia": "SERVICIO NEONATOLOGIA-EMERGENCIA"
+    }, 
+    {
+        "documento": 10000002, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 101, 
+        "dependencia": "DIRECCION GENERAL DE FISCALIZACION SANITARIA"
+    }, 
+    { 
+        "documento": 10000003, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 102, 
+        "dependencia": "SERVICIOS GENERALES" 
+    },
+    { 
+        "documento": 10000004, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 103, 
+        "dependencia": "UNIDAD NEONATOLOGIA" 
+    }, 
+    { 
+        "documento": 10000005, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 104, 
+        "dependencia": "AO AGUILARES" 
+    }, 
+    { 
+        "documento": 10000006, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 105, 
+        "dependencia": "UNIDAD LABORATORIO CENTRAL" 
+    }, 
+    { 
+        "documento": 10000007, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 106, 
+        "dependencia": "SALA V" 
+    }, 
+    { 
+        "documento": 10000008, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 107, 
+        "dependencia": "CAPS SAN BERNARDO" 
+    }, 
+    { 
+        "documento": 10000009, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 108, 
+        "dependencia": "DIRECCION HOSPITAL DE CLINICAS NUESTRA SEÑORA DEL CARMEN" 
+    }, 
+    { 
+        "documento": 10000010, 
+        "nombre": "JUAN AGUSTIN", 
+        "apellido": "SANCHEZ", 
+        "email": "JUANAGUSTINSANCHEZ@gmail.com", 
+        "telefono": 123456789, 
+        "iddependencia": 109, 
+        "dependencia": "OBSTETRICIA" 
+    }
 ]
 export default datos;
